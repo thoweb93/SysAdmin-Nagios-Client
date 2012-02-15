eb1d2098fdfe61a85bf8b1766b11c0872a5e8bbe
Lukas Bernreiter <lukas.bernreiter@gmail.com>
2012-02-15 14:53:29 +0100

    added

---
+++ src/com/SysAdmin/SysAdminActivity.java
@@ -0,0 +1,13 @@
+package com.SysAdmin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SysAdminActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
