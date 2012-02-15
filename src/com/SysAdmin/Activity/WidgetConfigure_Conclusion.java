9f558442bbc8c24a89b1b64ea70430a9ed5e6973
Lukas Bernreiter <lukas.bernreiter@gmail.com>
2012-02-15 16:33:57 +0100

    -

--- src/com/SysAdmin/Activity/WidgetConfigure_Conclusion.java
+++ src/com/SysAdmin/Activity/WidgetConfigure_Conclusion.java
@@ -1,5 +1,26 @@
 package com.SysAdmin.Activity;
 
-public class WidgetConfigure_Conclusion {
 
+import com.SysAdmin.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WidgetConfigure_Conclusion extends Activity {
+	
+	public WidgetConfigure_Conclusion()
+	{
+		super();
+	}
+	
+	public void onCreate(Bundle _icicle)
+	{
+		super.onCreate(_icicle);
+		
+		// set layout
+		this.setContentView(R.layout.conclusion);			
+		
+		
+	}	
+	
 }
